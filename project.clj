a65be39dc10edf92ac862001ba426f995c860738
Paul deGrandis <paul.degrandis@gmail.com>
2012-09-28 20:44:31 -0700

    project updates

---
+++ project.clj
@@ -0,0 +1,22 @@
+(defproject ttt "0.1.0-SNAPSHOT"
+  :description "git-backed text-based ticket tracker"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/core.logic "0.8-alpha3"]]
+  :plugins [[lein-cljsbuild "0.2.7"]
+             [jonase/kibit "0.0.4"]
+             [lein-catnip "0.4.1"]]
+
+  ;; This project current needs patches which are not landed in CLJS master
+  ;; These settings are here for future reference.
+  ;; Please use the `compile` script
+  :cljsbuild {:builds [{:source-path "src",
+                        :compiler {:output-dir "build/cljs",
+                                   :output-to "build/ttt",
+                                   :optimizations :advanced,;:advanced ;:simple ;:whitespace
+                                   :pretty-print false
+                                   :target :nodejs}}]} 
+  :warn-on-reflection false)
+
